d8137ca1806fd49729c508b6f9486df6660b5081
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-28 20:33:16 +0700

    add module and init project

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,47 +1,31 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+
+declare(strict_types=1);
+
+/*
+ * Copyright © 2019 Dxvn, Inc. All rights reserved.
+ *
+ * © Tran Ngoc Duc <ductn@diepxuan.com>
+ *   Tran Ngoc Duc <caothu91@gmail.com>
  */
 
-/**
- * PHP Coding Standards fixer configuration
- */
+if (!function_exists('import_url')) {
+    function import_url($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
 
-$finder = PhpCsFixer\Finder::create()
-    ->name('*.phtml')
-    ->exclude('dev/tests/integration/tmp')
-    ->exclude('dev/tests/integration/var')
-    ->exclude('lib/internal/Cm')
-    ->exclude('lib/internal/Credis')
-    ->exclude('lib/internal/Less')
-    ->exclude('lib/internal/LinLibertineFont')
-    ->exclude('pub/media')
-    ->exclude('pub/static')
-    ->exclude('setup/vendor')
-    ->exclude('var');
+        return $data;
+    }
+}
 
-$config = new PhpCsFixer\Config();
-$config->setFinder($finder)
-    ->setRules([
-        '@PSR2' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'concat_space' => ['spacing' => 'one'],
-        'include' => true,
-        'new_with_braces' => true,
-        'no_empty_statement' => true,
-        'no_extra_blank_lines' => true,
-        'no_leading_import_slash' => true,
-        'no_leading_namespace_whitespace' => true,
-        'no_multiline_whitespace_around_double_arrow' => true,
-        'multiline_whitespace_before_semicolons' => true,
-        'no_singleline_whitespace_before_semicolons' => true,
-        'no_trailing_comma_in_singleline_array' => true,
-        'no_unused_imports' => true,
-        'no_whitespace_in_blank_line' => true,
-        'object_operator_without_whitespace' => true,
-        'ordered_imports' => true,
-        'standardize_not_equals' => true,
-        'ternary_operator_spaces' => true,
-    ]);
-return $config;
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$config = import_url('https://raw.githubusercontent.com/diepxuan/php/main/.php-cs-fixer.dist.php');
+$config = str_replace('<?php', '', $config);
+
+return eval($config);
