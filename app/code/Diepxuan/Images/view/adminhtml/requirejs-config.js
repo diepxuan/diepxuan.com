43f2718469f2f3ded5a49485d4f8d42c7e0bc5db
Tran Ngoc Duc <caothu91@gmail.com>
2025-04-04 10:18:18 +0700

    remove images module

--- app/code/Diepxuan/Images/view/adminhtml/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Backend/js/media-uploader': {
-                'Diepxuan_Images/js/media-uploader-mixin': true
-            }
-        }
-    }
-};
